nette/nette

Database: database extensible by row factory

Nette/Database/Table/Selection.php
@@ -493,7 +493,12 @@ class Selection extends Nette\Object implements \Iterator, \ArrayAccess, \Counta
 
 	protected function createRow(array $row)
 	{
-		return new ActiveRow($row, $this);
+		if ($factory = $this->connection->getRowFactory()) {
+			return $factory->createRow($row, $this);
+
+		} else {
+			return new ActiveRow($row, $this);
+		}
 	}
 
 

juzna/nette-database

src/Database/Table/Selection.php
@@ -520,7 +520,12 @@ class Selection extends Nette\Object implements \Iterator, IRowContainer, \Array
 
 	protected function createRow(array $row)
 	{
-		return new ActiveRow($row, $this);
+		if ($factory = $this->context->getRowFactory()) {
+			return $factory->createRow($row, $this);
+
+		} else {
+			return new ActiveRow($row, $this);
+		}
 	}
 
 